1f1a8003ae8c4b72666f487975864deee8b1a295
Saket Bihary <saketbihary123@gmail.com>
2026-05-01 12:58:38 +0530

    fix

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -4,7 +4,7 @@
 
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+// import { useAuth } from '../context/AuthContext';
 
 export default function LoginPage() {
   // await login(form.email, form.password);
